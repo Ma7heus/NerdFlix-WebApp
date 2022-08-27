16a9bbb3cb1711c3af44da74c8647fb986779ad4
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-08-27 15:06:43 -0300

    Adicionando Alteracoes

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_tamplate, redirect
+from Clients import *
+from Products import *
+from Users import *
+
 app = Flask(__name__)
 
+@pp.route('/')
+def index():
+    title = "Bem vindo ao NerdFlix!"
+    return render_template("index.html", title=title)
 
 
 
 
 app.run(debuger=True)
+
 
 
 
