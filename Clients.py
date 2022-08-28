ecabae61bb423f8b1cd46a8c3a4613b3c8c4815e
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-08-27 21:00:27 -0300

    Continuando projeto em novo repositorio

---
+++ Clients.py
@@ -0,0 +1,6 @@
+class Client:
+    def __init__(self, ID, name, lastName, cart):
+        self.ID = ID
+        self.name = name
+        self.lastName = lastName
+        self.cart = cart
---
+++ Products.py
@@ -0,0 +1,9 @@
+class Products:
+    def __init__(self, code, name, value, type, canBuy):
+        self.code = code
+        self.name = name
+        self.value = value
+        self.type = type
+        self.canBuy = canBuy
+
+
