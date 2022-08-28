1f59e551eef1b971ec7e2c5671b8f90f4887afcc
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-08-28 16:08:44 -0300

    Adicionando novos metodos para buscar dados

--- Users.py
+++ Users.py
@@ -5,13 +5,35 @@ class Users:
         self.password = password
         self.nickName = nickName
 
-def getUser(tipo):
-    dados = getData("users")
-    if tipo == "value":
-        listUsers = list(dados.values())
-    elif tipo == "key":
-        listUsers = list(dados.keys())
-    return listUsers
-    
+    def getUser(tipo):
+        dados = getData("users")
+        if tipo == "value":
+            listUsers = list(dados.values())
+        elif tipo == "key":
+            listUsers = list(dados.keys())
+        return listUsers
 
+    def getName():
+        userValues = Users.getUser("value")
+        listName = []
+        for i in userValues:
+            name = i['name']
+            listName.append(name)
+        return listName
+
+    def getNickName():
+        userValues = Users.getUser("value")
+        listNickName = []
+        for i in userValues:
+            nickName = i['nickName']
+            listNickName.append(nickName)
+        return listNickName
+
+    def getPassword():
+        userValues = Users.getUser("value")
+        listPassword = []
+        for i in userValues:
+            password = i['password']
+            listPassword.append(password)
+        return listPassword
 
