7366f0fd613840fd8346bc076fefed44911e86e9
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-08-29 22:41:07 -0300

    Adiconando novos metodos de products

--- Products.py
+++ Products.py
@@ -1,9 +1,30 @@
+from functions import *
 class Products:
     def __init__(self, code, name, value, type, canBuy):
         self.code = code
         self.name = name
         self.value = value
-        self.type = type
+        self.type = ptype
         self.canBuy = canBuy
+    
+    
+    def getAllProducts(tipo):
+        dados = getData("products")
+        if tipo == "value":
+            listProducts = list(dados.values())
+        elif tipo == "key":
+            listProducts = list(dados.keys())
+        return listProducts
+
+
+
+    def getProduct():
+        return
+    def getMovies():
+        return
+    def getSeries():
+        return
+    def getDocumentary():
+        return
 
 
