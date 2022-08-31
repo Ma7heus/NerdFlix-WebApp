765a1ea3595374144d956397719ac48fc25cb593
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-08-31 02:40:11 -0300

    Adicionando tratamento para dados em metodo da classe Products

--- Products.py
+++ Products.py
@@ -12,8 +12,33 @@ class Products:
         dados = getData("products")
         if tipo == "value":
             listProducts = list(dados.values())
+
+            #tratando dados dentro do dicionario
+            tipoProduct = None
+            for i in listProducts:
+                tipo = i["tipo"]
+                if tipo == 1:
+                    tipoProduct = "Serie"        
+                    i.update({"tipo":tipoProduct})                    
+                elif tipo == 2:
+                    tipoProduct = "Movie"       
+                    i.update({"tipo":tipoProduct})
+                elif tipo == 3:
+                    tipoProduct = "Documentary"       
+                    i.update({"tipo":tipoProduct})
+                else:
+                    tipoProduct = "unknown"       
+                    i.update({"tipo":tipoProduct})  
+            for i in listProducts:
+                if i["canBuy"] == True:
+                    i.update({"canBuy": "Available"})
+                elif i["canBuy"] == False:
+                    i.update({"canBuy": "Unavailable"})
+                else:
+                    i.update({"canBuy": "unknown"})     
         elif tipo == "key":
-            listProducts = list(dados.keys())
+            listProducts = list(dados.keys())       
+                
         return listProducts
 
 
